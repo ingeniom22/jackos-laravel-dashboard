b46222a5e38eec70c026588a2623cb45e94dab9e
ingeniom22 <jamesmichael0444@gmail.com>
2023-12-12 12:52:27 +0700

    final migrations

---
+++ database/migrations/2023_12_12_055110_create_transaction_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('service_id');
+            $table->unsignedBigInteger('employee_id');
+            $table->unsignedBigInteger('payment_id');
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->foreign('payment_id')->references('id')->on('payments');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
