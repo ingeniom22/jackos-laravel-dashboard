eb7d394f42290f5af7f912c1b2355471c31b1dd2
ingeniom22 <jamesmichael0444@gmail.com>
2023-12-15 14:48:33 +0700

    complete controllers

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,6 +9,45 @@ class PaymentController extends Controller
 {
     public function index()
     {
-        return view('payment', ['payments' => Payment::all()]);
+        return view('payment.index', ['payment' => Payment::all()]);
+    }
+
+    public function create()
+    {
+        return view('payment.create');
+    }
+
+    public function store(Request $request)
+    {
+        Payment::create($request->all());
+
+        return redirect()->route('payment.index')
+            ->with('success', 'Payment created successfully.');
+    }
+
+    public function show(Payment $payment)
+    {
+        return view('payment.show', ['payment' => $payment]);
+    }
+
+    public function edit(Payment $payment)
+    {
+        return view('payment.edit', ['payment' => $payment]);
+    }
+
+    public function update(Request $request, Payment $payment)
+    {
+        $payment->update($request->all());
+
+        return redirect()->route('payment.index')
+            ->with('success', 'Payment updated successfully.');
+    }
+
+    public function destroy(Payment $payment)
+    {
+        $payment->delete();
+
+        return redirect()->route('payment.index')
+            ->with('success', 'Payment deleted successfully.');
     }
 }
