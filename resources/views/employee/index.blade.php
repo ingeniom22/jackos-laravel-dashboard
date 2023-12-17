d410d2dc1f803c73986a9322e406e3e9e959fb0a
Fauzan <fauzanvictor04@gmail.com>
2023-12-17 21:57:36 +0700

    rearrange crud

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,50 +1,52 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="{{ route('employee.create')}}" class="btn btn-accent text-white mb-4">
-                        Add Employee
-                    </a>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-2 text-gray-900">
+            <div class="py-1">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            <a href="{{ route('employee.create')}}" class="btn btn-accent text-white mb-4">
+                                Add Employee
+                            </a>
 
-                    <table class="min-w-full border rounded-lg overflow-hidden">
-                        <thead class="bg-gradient-to-r from-teal-400 to-teal-600 text-white">
-                            <tr>
-                                <th class="w-1/6 py-2 px-4 border">ID</th>
-                                <th class="w-1/6 py-2 px-4 border">Employee Name</th>
-                                <th class="w-1/6 py-2 px-4 border">Email</th>
-                                <th class="w-1/6 py-2 px-4 border">Phone Number</th>
-                                <th class="w-1/6 py-2 px-4 border">Contract Start Date</th>
-                                <th class="w-1/6 py-2 px-4 border">Contract End Date</th>
-                                <th class="w-1/6 py-2 px-4 border">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($employee as $employee)
-                            <tr>
-                                <td class="w-1/6 py-2 px-4 border">{{ $employee->id}}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $employee->employee_name }}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $employee->email}}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $employee->phone_no}}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $employee->contract_start_date }}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $employee->contract_end_date }}</td>
-                                <td class="w-1/6 py-2 px-4 border">
-                                    <div class="btn-group" role="group">
-                                        <a href="{{ route('employee.edit', $employee->id) }}" class="btn btn-warning text-white">Edit</a>
-                                        <form action="{{ route('employee.destroy', $employee->id) }}" method="POST" class="inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-error text-white">Delete</button>
-                                        </form>
-                                    </div>
-                                </td>
+                            <table class="min-w-full border rounded-lg overflow-hidden text-center">
+                                <thead class="bg-gradient-to-r from-teal-400 to-teal-600 text-white">
+                                    <tr>
+                                        <th class="w-1/6 py-3 px-4 border">ID</th>
+                                        <th class="w-1/6 py-3 px-4 border">Employee Name</th>
+                                        <th class="w-1/6 py-3 px-4 border">Email</th>
+                                        <th class="w-1/6 py-3 px-4 border">Phone Number</th>
+                                        <th class="w-1/6 py-3 px-4 border">Contract Start Date</th>
+                                        <th class="w-1/6 py-3 px-4 border">Contract End Date</th>
+                                        <th class="w-1/6 py-3 px-4 border">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($employee as $employee)
+                                    <tr>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $employee->id}}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $employee->employee_name }}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $employee->email}}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $employee->phone_no}}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $employee->contract_start_date }}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $employee->contract_end_date }}</td>
+                                        <td class="w-1/6 py-2 px-4 border">
+                                            <div class="btn-group" role="group">
+                                                <a href="{{ route('employee.edit', $employee->id) }}" class="btn btn-warning text-white">Edit</a>
+                                                <form action="{{ route('employee.destroy', $employee->id) }}" method="POST" class="inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-error text-white">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
 
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 </x-app-layout>
--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,48 +1,50 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="{{ route('payment.create')}}" class="btn btn-accent text-white mb-4">
-                        Add Payment
-                    </a>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-2 text-gray-900">
+            <div class="py-1">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            <a href="{{ route('payment.create')}}" class="btn btn-accent text-white mb-4">
+                                Add Payment
+                            </a>
 
-                    <table class="min-w-full border rounded-lg overflow-hidden">
-                        <thead class="bg-gradient-to-r from-teal-400 to-teal-600 text-white">
-                            <tr>
-                                <th class="w-1/6 py-2 px-4 border">ID</th>
-                                <th class="w-1/6 py-2 px-4 border">Payment Name</th>
-                                <th class="w-1/6 py-2 px-4 border">Payment Description</th>
-                                <th class="w-1/6 py-2 px-4 border">Fee</th>
-                                <th class="w-1/6 py-2 px-4 border">Actions</th>
+                            <table class="min-w-full border rounded-lg overflow-hidden text-center">
+                                <thead class="bg-gradient-to-r from-teal-400 to-teal-600 text-white">
+                                    <tr>
+                                        <th class="w-1/6 py-3 px-4 border">ID</th>
+                                        <th class="w-1/6 py-3 px-4 border">Payment Name</th>
+                                        <th class="w-1/6 py-3 px-4 border">Payment Description</th>
+                                        <th class="w-1/6 py-3 px-4 border">Fee</th>
+                                        <th class="w-1/6 py-3 px-4 border">Actions</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($payment as $payment)
-                            <tr>
-                                <td class="w-1/6 py-2 px-4 border">{{ $payment->id}}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $payment->payments }}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $payment->description}}</td>
-                                <td class="w-1/6 py-2 px-4 border">{{ $payment->fee}}</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($payment as $payment)
+                                    <tr>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $payment->id}}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $payment->payments }}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $payment->description}}</td>
+                                        <td class="w-1/6 py-2 px-4 border">{{ $payment->fee}}</td>
 
-                                <td class="w-1/6 py-2 px-4 border">
-                                    <div class="btn-group" role="group">
-                                        <a href="{{ route('payment.edit', $payment->id) }}" class="btn btn-warning text-white">Edit</a>
-                                        <form action="{{ route('payment.destroy', $payment->id) }}" method="POST" class="inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-error text-white">Delete</button>
-                                        </form>
-                                    </div>
-                                </td>
+                                        <td class="w-1/6 py-2 px-4 border">
+                                            <div class="btn-group" role="group">
+                                                <a href="{{ route('payment.edit', $payment->id) }}" class="btn btn-warning text-white">Edit</a>
+                                                <form action="{{ route('payment.destroy', $payment->id) }}" method="POST" class="inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-error text-white">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
 
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 </x-app-layout>
