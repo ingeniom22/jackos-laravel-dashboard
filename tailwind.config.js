b07fd32ec58631613f2f3a0648b9ce8fc6c4fc31
ingeniom22 <jamesmichael0444@gmail.com>
2023-12-17 11:14:02 +0700

    try navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/**/*.{js,blade.php}",
+        "./app/View/Components/**/**/*.php",
+        "./app/Livewire/**/**/*.php",
     ],
 
     theme: {
